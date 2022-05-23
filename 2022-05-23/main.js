3311c3ebfd7877e08e98d82ed93d15a816beccaa
Sylvio <sylviomafort@gmail.com>
2022-05-23 22:51:18 +0900

    2022-05-23 22:51

---
+++ 2022-05-23/main.js
@@ -0,0 +1,12 @@
+// Filling an array (part 1)
+const arr = (N) => {
+  let arr = [];
+
+  for (let i = 0; i < N; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+
+  //   [ the numbers 0 to N-1 ];
+};
