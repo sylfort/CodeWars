0e6de9dd36eacda5741b94c40892041c31ef9282
Sylvio <sylviomafort@gmail.com>
2022-07-08 22:40:24 +0900

    2022-07-08 22:40

---
+++ 2022-07-08/main.js
@@ -0,0 +1,6 @@
+//Looking for a benefactor
+function newAvg(arr, newavg) {
+  const x = newavg * (arr.length + 1) - arr.reduce((a, b) => a + b, 0);
+  if (x <= 0) throw "ERROR";
+  return Math.ceil(x);
+}
