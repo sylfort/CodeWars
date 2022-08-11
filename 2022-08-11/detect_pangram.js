e98ba234bc080aa49c413ff9261aeed5965aebe2
Sylvio <sylviomafort@gmail.com>
2022-08-11 20:14:44 +0900

    2022-08-11 20:14

---
+++ 2022-08-11/detect_pangram.js
@@ -0,0 +1,43 @@
+function isPangram(string){
+    let alphabet = {
+      a: 0,
+      b: 0,
+      c: 0,
+      d: 0,
+      e: 0,
+      f: 0,
+      g: 0,
+      h: 0,
+      i: 0,
+      j: 0,
+      k: 0,
+      l: 0,
+      m: 0,
+      n: 0,
+      o: 0,
+      p: 0,
+      q: 0,
+      r: 0,
+      s: 0,
+      t: 0,
+      u: 0,
+      v: 0,
+      w: 0,
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    
+    let arrString = string.toLowerCase().split("").filter(char => /[a-zA-Z]/.test(char));
+    
+    arrString.map(e=> (!alphabet[e]) ? alphabet[e] = 1 : false);
+    
+    for(const key in alphabet){
+      if(!alphabet[key]){
+        return false;
+      }
+    }
+    
+    return true;
+    
+  }
