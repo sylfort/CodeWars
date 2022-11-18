a9bc60f8c3ef2a5b8c0e381f3c2c0a4161866887
Sylvio <sylviomafort@gmail.com>
2022-11-18 22:11:08 +0900

    2022-11-18 22:11

---
+++ 2022-11-18/multiples_of_a_number.ts
@@ -0,0 +1,9 @@
+export function findMultiples(integer: number, limit: number): number[] {
+  let arr: number[] = [];
+  for (let i = integer; i <= limit; i++) {
+    if (i % integer == 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
