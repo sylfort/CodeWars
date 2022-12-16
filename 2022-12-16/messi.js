d8c85f0c2cb6185aafd81f785de83e7c13ef3783
Sylvio <sylviomafort@gmail.com>
2022-12-16 20:44:53 +0900

    2022-12-16 20:44

---
+++ 2022-12-16/messi.js
@@ -0,0 +1,5 @@
+// 8 kyu Grasshopper - Messi goals function
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
