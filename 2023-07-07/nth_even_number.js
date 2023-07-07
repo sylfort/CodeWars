61c42a032d1f5fe7e522f86d7f0c882b73a9e26f
Sylvio <sylviomafort@gmail.com>
2023-07-07 22:14:49 +0900

    2023-07-07 22:14

---
+++ 2023-07-07/nth_even_number.js
@@ -0,0 +1,11 @@
+// 8 kyu Get Nth Even Number
+
+function nthEven(n) {
+  //   let final = 0;
+  //   for(let i=1;i<n;i++){
+  //     final = final + 2;
+  //   }
+  //   return final;
+
+  return 2 * (n - 1);
+}
