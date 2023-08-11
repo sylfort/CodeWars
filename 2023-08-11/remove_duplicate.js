0a1c66d8c1e5fa5caca63cf4701635ec48a4ee0b
Sylvio <sylviomafort@gmail.com>
2023-08-11 22:39:15 +0900

    2023-08-11 22:39

---
+++ 2023-08-11/remove_duplicate.js
@@ -0,0 +1,12 @@
+// 8 kyu Remove duplicates from list
+
+function distinct(arr) {
+    let res = []; 
+    
+    for(let i = 0; i < arr.length; i++){
+      if(!res.includes(arr[i])){
+        res.push(arr[i]);
+      }
+  }
+  return res;
+  }
