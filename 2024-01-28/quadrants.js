ca9f937bd2e51659a818187e0acbafcbdb4dabbb
Sylvio <sylviomafort@gmail.com>
2024-01-28 23:46:46 +0900

    2024-01-28 23:46

---
+++ 2024-01-28/quadrants.js
@@ -0,0 +1,13 @@
+// 8 kyu Quadrants
+
+function quadrant(x, y) {
+  if ((x > 0) & (y > 0)) {
+    return 1;
+  } else if ((x < 0) & (y > 0)) {
+    return 2;
+  } else if ((x < 0) & (y < 0)) {
+    return 3;
+  } else {
+    return 4;
+  }
+}
